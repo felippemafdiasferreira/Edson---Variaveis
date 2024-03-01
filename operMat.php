44b48a89aee602ce86bfa04d4f7a429e02ef9dee
felippemafdiasferreira <129065606+felippemafdiasferreira@users.noreply.github.com>
2024-03-01 12:05:36 -0300

    Add files via upload

---
+++ operMat.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Operadores Matemáticos em PHP</title>
+</head>
+<body>
+    <?php
+    echo "<h3>OPERADORES MATEMÁTICOS EM PHP</h3>";
+    // as 2 variaveis abaixo são do tipo inteiro
+    $valor1 = 50;
+    $valor2 = 20;
+    $soma = $valor1
+    $soma = $valor2
+    echo "Valor1:" .$valor1
+    echo
+    ?>
+</body>
+</html>
